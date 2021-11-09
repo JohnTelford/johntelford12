7d08e2cb80eb478e0d30e38c83eb68cba49345c2
John <john@johntelford.com>
2021-11-09 12:29:58 -0800

    "Ideas" edits
    Signed-off-by: John <john@johntelford.com>

--- components/header.js
+++ components/header.js
@@ -47,7 +47,8 @@ export default function Header() {
             { title: "Deploy", route: "/Iterative/Deploy/deploy"},
             { title: "Maintain", route: "/Iterative/Maintain/maintain" },
             { title: "Reference", route: "/Iterative/Develop/webtechrefs"},
-            { title: "Company", route: "/Iterative/Company/company" }
+            { title: "Company", route: "/Iterative/Company/company" },
+            { title: "Ideas", route: "/Iterative/Ideas/ideas"}
           ].map(({ route, title }) => (
             <li className="mt-3 md:mt-0 md:ml-6" key={title}>
               <Link href={route}>
